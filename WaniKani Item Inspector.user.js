0432fe35eb46762bcbfdb93f0410512b61633c79
rouleaup88 <73247566+rouleaup88@users.noreply.github.com>
2023-11-26 15:34:10 -0500

    Update WaniKani Item Inspector.user.js

--- WaniKani Item Inspector.user.js
+++ WaniKani Item Inspector.user.js
@@ -3,7 +3,7 @@
 // @namespace     wk-dashboard-item-inspector
 // @description   Inspect Items in Tabular Format
 // @author        prouleau
-// @version       1.26.2
+// @version       1.26.3
 // @match         https://www.wanikani.com/dashboard
 // @match         https://www.wanikani.com/
 // @copyright     2020+, Paul Rouleau
